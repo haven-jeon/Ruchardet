296083e54876c1bae553262ccc06d02b51cbf541
gogamza <madjakarta@gmail.com>
2013-11-19 02:00:32 +0900

    remove unused line

--- src/getCharEncoding.cpp
+++ src/getCharEncoding.cpp
@@ -4,25 +4,21 @@
 
 
 extern "C"  SEXP getCharEncoding(SEXP chars){
-//Rcpp::CharacterVector getCharEncoding(Rcpp::CharacterVector chars){
   Rcpp::StringVector charssv(chars);
   int inputsize = charssv.size();
-  
+
   uchardet_t encoding_detector = uchardet_new();
   
-  
   Rcpp::CharacterVector ouputs(inputsize);
-  
+
   for(int i = 0; i< inputsize; i++){  
     uchardet_handle_data(encoding_detector, charssv[i], strlen(charssv[i]));  
     uchardet_data_end(encoding_detector);
     ouputs[i]  = uchardet_get_charset(encoding_detector);
-    std::cout  << ouputs[i];
     uchardet_reset(encoding_detector);
   }
-  
   uchardet_delete(encoding_detector);
-  
+
   return ouputs;
 } 
 
