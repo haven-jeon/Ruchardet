816190eb985faeb8da8832c67c2b4cdc658c694c
gogamza <madjakarta@gmail.com>
2013-11-21 00:09:47 +0900

    pass 'R CMD check'

--- src/getCharEncoding.cpp
+++ src/getCharEncoding.cpp
@@ -1,18 +1,25 @@
 #include <Rcpp.h>
 #include <string.h>
-#include "uchardet/uchardet.h"
+#include <uchardet/uchardet.h>
+
+using namespace Rcpp;
 
 
-extern "C"  SEXP getCharEncoding(SEXP chars){
-  Rcpp::StringVector charssv(chars);
-  int inputsize = charssv.size();
+
+
+//' get encoding information of input string 
+//' @param str input string
+//' @return string of encoding 
+// [[Rcpp::export]]
+CharacterVector getCharEncoding(StringVector str){
+  int inputsize = str.size();
 
   uchardet_t encoding_detector = uchardet_new();
   
-  Rcpp::CharacterVector ouputs(inputsize);
+  CharacterVector ouputs(inputsize);
 
   for(int i = 0; i< inputsize; i++){  
-    uchardet_handle_data(encoding_detector, charssv[i], strlen(charssv[i]));  
+    uchardet_handle_data(encoding_detector, str[i], strlen(str[i]));  
     uchardet_data_end(encoding_detector);
     ouputs[i]  = uchardet_get_charset(encoding_detector);
     uchardet_reset(encoding_detector);
